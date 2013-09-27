706284c2c360232209ce8180a9d8aeeb913524ab
Rhys van der Waerden <rhys.vdw@gmail.com>
2013-09-28 00:39:36 +1000

    Rotating birds

--- Assets/Bird/Scripts/BirdRotationController.cs
+++ Assets/Bird/Scripts/BirdRotationController.cs
@@ -5,7 +5,16 @@ public class BirdRotationController : Behavior
 {
     void LateUpdate()
     {
-        var dir = transform.parent.rotation.eulerAngles.x;
-        print( (dir - 270f) );
+        var dir = transform.parent.rotation.eulerAngles.x - 180f;
+        print( dir );
+        var up = Quaternion.Euler( dir, 0f, 0f ) * Vector3.up;
+        if( up.y < 0 ) { up = up.WithY( -up.y ); }
+        transform.rotation = Quaternion.LookRotation(
+            transform.parent.forward,
+            up
+        );
+
+        Debug.DrawRay( transform.position, up * 10f );
+        //transform.localRotation = Quaternion.Euler( Vector3.forward * dir );
     }
 }
